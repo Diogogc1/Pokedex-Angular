13caa98c82c8281d87f9cfe145cb05127540d30b
Diogo Gomes <diogogomescastro@outlook.com>
2024-01-18 12:52:23 -0300

    Cooldown Click

--- pokedex/src/app/app.component.ts
+++ pokedex/src/app/app.component.ts
@@ -11,64 +11,163 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./app.component.css', './app.componentMobile.css'],
 })
 export class AppComponent implements OnInit{
+
+  //INICIALIZAÇÃO DO COMPONENTE
   ngOnInit(): void {
     this.getInfoPokemon();
   }
-  mostrarSombraPesquisa:boolean = true;
-  mostrarSombraAnterior:boolean = true;
-  mostrarSombraProximo:boolean = true;
-  inputPesquisa:string = '1';
-  title = 'pokedex';
-  id:number = 1;
-  nomePokemon:string = 'bulbassaur';
-  pokemonImg:string = '';
-  
-  async getInfoPokemon(): Promise<void> {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${this.inputPesquisa}/`);
-    const data = await response.json();
-    this.id = data.id;
-    this.nomePokemon = data.name;
-    this.pokemonImg = data.sprites.front_default;
-    this.inputPesquisa = '';
 
-    this.mostrarSombraPesquisa = false;
+  //VARIAVEIS
+  private _mostrarSombraPesquisa:boolean = true;
+  private _mostrarSombraAnterior:boolean = true;
+  private _mostrarSombraProximo:boolean = true;
+  private _inputPesquisa:string = '1';
+  private _title = 'pokedex';
+  private _id:number = 1;
+  private _nomePokemon:string = 'bulbassaur';
+  private _pokemonImg:string = '';
+  private _tempoDeEspera:boolean = true;
+
+  //METÓDOS
+  async getInfoPokemon(): Promise<void> {
+    if(!this._tempoDeEspera){
+      return;
+    }
+    this._tempoDeEspera = false;
+    this._inputPesquisa = this._inputPesquisa.trim().toLowerCase();
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${this._inputPesquisa}/`);
+    const data = await response.json();
+    this._id = data.id;
+    this._nomePokemon = data.name;
+    this._pokemonImg = data.sprites.front_default;
+    this._inputPesquisa = '';
+
+    this._mostrarSombraPesquisa = false;
 
     setTimeout(() => {
-      this.mostrarSombraPesquisa = true;
+      this._mostrarSombraPesquisa = true;
     }, 300);
+
+    setTimeout(() => {
+      this._tempoDeEspera = true;
+    }, 500);
   }
 
   async getProximo(): Promise<void> {
-    if(this.id != 150){
-      this.id++;
+    if(this.id <= 700 && this.tempoDeEspera){
+      this._tempoDeEspera = false;
+      this._id++;
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${this.id}/`);
+      const data = await response.json();
+      this._id = data.id;
+      this._nomePokemon = data.name;
+      this._pokemonImg = data.sprites.front_default;
+
+      this._mostrarSombraProximo = false;
+
+      setTimeout(() => {
+        this._mostrarSombraProximo = true;
+      }, 300);
+
+      setTimeout(() => {
+        this._tempoDeEspera = true;
+      }, 500);
     }
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${this.id}/`);
-    const data = await response.json();
-    this.id = data.id;
-    this.nomePokemon = data.name;
-    this.pokemonImg = data.sprites.front_default;
-
-    this.mostrarSombraProximo = false;
-
-    setTimeout(() => {
-      this.mostrarSombraProximo = true;
-    }, 300);
   }
 
   async getAnterior(): Promise<void> {
-    if(this.id != 1){
-      this.id--;
+    if(this.id != 1 && this.tempoDeEspera){
+      this.tempoDeEspera = false;
+      this._id--;
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${this._id}/`);
+      const data = await response.json();
+      this._id = data.id;
+      this._nomePokemon = data.name;
+      this._pokemonImg = data.sprites.front_default;
+
+      this._mostrarSombraAnterior = false;
+
+      setTimeout(() => {
+        this._mostrarSombraAnterior = true;
+      }, 300);
+
+      setTimeout(() => {
+        this.tempoDeEspera = true;
+      }, 500);
     }
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${this.id}/`);
-    const data = await response.json();
-    this.id = data.id;
-    this.nomePokemon = data.name;
-    this.pokemonImg = data.sprites.front_default;
+  }
 
-    this.mostrarSombraAnterior = false;
 
-    setTimeout(() => {
-      this.mostrarSombraAnterior = true;
-    }, 300);
+  /*GETTERS E SETTERS*/
+  get mostrarSombraPesquisa() {
+    return this._mostrarSombraPesquisa
+  }
+  
+  set mostrarSombraPesquisa(val: boolean) {
+    this._mostrarSombraPesquisa = val
+  }
+  
+  get mostrarSombraAnterior() {
+    return this._mostrarSombraAnterior
+  }
+  
+  set mostrarSombraAnterior(val: boolean) {
+    this._mostrarSombraAnterior = val
+  }
+  
+  get mostrarSombraProximo() {
+    return this._mostrarSombraProximo
+  }
+  
+  set mostrarSombraProximo(val: boolean) {
+    this._mostrarSombraProximo = val
+  }
+  
+  get inputPesquisa() {
+    return this._inputPesquisa
+  }
+  
+  set inputPesquisa(val: string) {
+    this._inputPesquisa = val
+  }
+  
+  get title() {
+    return this._title
+  }
+  
+  set title(val: any) {
+    this._title = val
+  }
+  
+  get id() {
+    return this._id
+  }
+  
+  set id(val: number) {
+    this._id = val
+  }
+  
+  get nomePokemon() {
+    return this._nomePokemon
+  }
+  
+  set nomePokemon(val: string) {
+    this._nomePokemon = val
+  }
+  
+  get pokemonImg() {
+    return this._pokemonImg
+  }
+  
+  set pokemonImg(val: string) {
+    this._pokemonImg = val
+  }
+  
+  get tempoDeEspera() {
+    return this._tempoDeEspera
+  }
+  
+  set tempoDeEspera(val: boolean) {
+    this._tempoDeEspera = val
   }
 }
